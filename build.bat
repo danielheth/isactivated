bff8f9e050e906acc3731ad5fb5ad1f59bfcbfc7
Daniel Moran <danielheth@hotmail.com>
2015-07-17 17:54:51 -0500

    adding signing step to build process

--- build.bat
+++ build.bat
@@ -2,6 +2,22 @@ set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
 call %msBuildDir%\msbuild.exe  isActivated.sln /p:Configuration=Release /l:FileLogger,Microsoft.Build.Engine;logfile=Manual_MSBuild_ReleaseVersion_LOG.log
 set msBuildDir=
 
+
+::ONLY SIGN IF THE CERT IS AVAILABLE
+if exist "C:\SignCert\MoranIT.pfx" (
+	signtool.exe sign /f "C:\SignCert\MoranIT.pfx" /p "M0r@n!T" /t "http://timestamp.verisign.com/scripts/timestamp.dll" "%WORKSPACE%\isActivated\bin\Release\isActivated.exe"
+) else (
+	echo Skipping signing since we're not on build server.
+)
+
+
 pushd isActivated\bin\Release\
 7za.exe a -tzip isactivated.zip isActivated.exe
-cd ../../../
+
+
+::ONLY SIGN IF THE CERT IS AVAILABLE
+if exist "C:\SignCert\MoranIT.pfx" (
+	signtool.exe sign /f "C:\SignCert\MoranIT.pfx" /p "M0r@n!T" /t "http://timestamp.verisign.com/scripts/timestamp.dll" "%WORKSPACE%\isActivated\bin\Release\isActivated.zip"
+) else (
+	echo Skipping signing since we're not on build server.
+)
