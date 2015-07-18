1076eee15fffc0a0988cddc2293a185a20396292
Daniel Moran <danielheth@hotmail.com>
2015-07-17 23:36:49 -0500

    adjusting signing

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ set msBuildDir=
 
 ::ONLY SIGN IF THE CERT IS AVAILABLE
 if exist "C:\SignCert\MoranIT.pfx" (
-	signtool.exe sign /f "C:\SignCert\MoranIT.pfx" /p "%SIGNING_CERT_PASSWORD%" /t "http://timestamp.verisign.com/scripts/timestamp.dll" "%WORKSPACE%\isActivated\bin\Release\isActivated.exe"
+	"C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\signtool.exe" sign /f "C:\SignCert\MoranIT.pfx" /p "%SIGNING_CERT_PASSWORD%" /t "http://timestamp.verisign.com/scripts/timestamp.dll" "%WORKSPACE%\isActivated\bin\Release\isActivated.exe"
 ) else (
 	echo Skipping signing since we're not on build server.
 )
@@ -18,7 +18,7 @@ pushd %WORKSPACE%\
 
 ::ONLY SIGN IF THE CERT IS AVAILABLE
 if exist "C:\SignCert\MoranIT.pfx" (
-	signtool.exe sign /f "C:\SignCert\MoranIT.pfx" /p "%SIGNING_CERT_PASSWORD%" /t "http://timestamp.verisign.com/scripts/timestamp.dll" "%WORKSPACE%\isActivated\bin\Release\isActivated.zip"
+	"C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\signtool.exe" sign /f "C:\SignCert\MoranIT.pfx" /p "%SIGNING_CERT_PASSWORD%" /t "http://timestamp.verisign.com/scripts/timestamp.dll" "%WORKSPACE%\isActivated\bin\Release\isActivated.zip"
 ) else (
 	echo Skipping signing since we're not on build server.
 )
